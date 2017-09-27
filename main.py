00b4c263e1e6216957daa975ed4d687dbfee209b
Priyanka <priyanka@Priyankas-MacBook-Pro-2.local>
2017-09-27 00:03:42 -0500

    now we have probability of each of the things. Next I will add classifying functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import pandas as pd
 import numpy as np
 
+mostFrequentClassOverall = -1
+
 def main():
     if len(sys.argv) != 3:
         print ("Please execute with two arguments <train.dat> <test.dat>")
@@ -12,7 +14,7 @@ def main():
         firstLine = trainingFile.readline()
         headers = firstLine.split()
 
-        print(headers)
+        #print(headers)
 
         trainingLists = []
         for line in trainingFile:
@@ -21,9 +23,44 @@ def main():
         # Convert training data into a pandas dataframe
         trainingDF = pd.DataFrame.from_records(trainingLists, columns=headers)
 
-        print(trainingDF)
+        #print(trainingDF)
+
 
         # Calculate most frequent class, set global instead of passing mostFrequentClassOverall or full dataset around to ever function
+        class1trainingDF = trainingDF.loc[trainingDF['class'] == '1']
+        print(len(class1trainingDF.index))
+        probOfClass1 = len(class1trainingDF.index)/len(trainingDF)
+        print(probOfClass1)
+        print ("P(C=1)=%.2f"%probOfClass1, end='')
+        for attribute in headers:
+            if attribute.lower() == "class":
+                continue
+            else:
+                attribute1GivenClass1 = class1trainingDF.loc[class1trainingDF[attribute] == '1']
+                probOfAttribute1GivenClass1 = len(attribute1GivenClass1.index)/len(class1trainingDF)
+                probOfAttribute0GivenClass1 = 1 - probOfAttribute1GivenClass1
+                print (' P(', attribute, '=1|C=1)=%.2f'%probOfAttribute1GivenClass1, end='')
+                print (' P(', attribute, '=0|C=1)=%.2f'%probOfAttribute0GivenClass1, end='')
+
+        print ()
+
+        class0trainingDF = trainingDF.loc[trainingDF['class'] == '0']
+        print(len(class0trainingDF.index))
+        probOfClass0 = len(class0trainingDF.index) / len(trainingDF)
+        print(probOfClass0)
+        print("P(C=0)=%.2f" % probOfClass0, end='')
+        for attribute in headers:
+            if attribute.lower() == "class":
+                continue
+            else:
+                attribute1GivenClass0 = class0trainingDF.loc[class0trainingDF[attribute] == '1']
+                probOfAttribute1GivenClass0 = len(attribute1GivenClass0.index) / len(class0trainingDF)
+                probOfAttribute0GivenClass0 = 1 - probOfAttribute1GivenClass0
+                print(' P(', attribute, '=1|C=0)=%.2f' % probOfAttribute1GivenClass0, end='')
+                print(' P(', attribute, '=0|C=0)=%.2f' % probOfAttribute0GivenClass0, end='')
+
+        print()
+        
         # total = 0
         # for each in trainingDF:
         #     total += int(each[headers.index("class")])
@@ -48,7 +85,7 @@ def main():
         # Convert test data into a pandas dataframe
         testDF = pd.DataFrame.from_records(testLists, columns=headers)
 
-        print(testDF)
+        #print(testDF)
 
         # Accuracy on test set
         # testAccuracy = getAccuracy(headNode, testLists)
@@ -57,3 +94,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
